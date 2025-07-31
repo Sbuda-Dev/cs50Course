39a47c8665438c9fb7f124259a2beb188c41ee37
Sibusiso <sbudamthimkhulu@gmail.com>
2025-07-31 04:08:58 +0200

    added um

---
+++ um/test_um.py
@@ -0,0 +1,9 @@
+import pytest
+from um import count
+
+def test_input():
+
+    assert count("Um, thanks for the cd.") == 1
+    assert count("um") == 1
+    assert count("Um, thanks, um, yeah") == 2
+    assert convert("Um?")
